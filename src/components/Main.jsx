d8e0cb4046e163b4e51769eb0b583cfe5e939000
JosephV95 <viltejl@gmail.com>
2023-08-04 12:48:02 -0300

    Se esta estilizando el main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,3 @@
-import reactLogo from "../assets/react.svg";
 import Slider from "./Slider/Slider";
 import "./main.css";
 
@@ -7,23 +6,24 @@ function Main() {
   return (
     <>
       <section>
+        <h4> <i>Find your perfect trip, designed by insiders who know and love their cities! </i></h4>
         <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-md-8 ">
-              <div className="article p-4 ">
+          <div className="row hero  justify-content-center align-items-center ">
+            <div className="hero-article col-md-8 ">
+              <div className=" p-3 my-2">
                 <h3>Find the perfect destination</h3>
                 <p>
                   Our app will help you find the perfect path for your next
                   trip. With an easy-to-use interface and a host of itinerary
                   options, planning your next trip has never been easier.
                 </p>
-                <button className="btn btn-info" type="button">
+                <button className="btn btn-info " type="button">
                   View More
                 </button>
               </div>
             </div>
-            <div className="col-md-4 d-flex justify-content-center">
-              <img src={reactLogo} alt="imgReact" />
+            <div className="col-md-4 hero-img d-flex justify-content-center p-1">
+              <img src="../kioto pagoda.jpg" alt="imgLogo" />
             </div>
           </div>
 
@@ -32,19 +32,16 @@ function Main() {
               Call to Action
             </button>
           </div>
-          <div className="row">
+          <div className="row my-5">
           <div className="col-12">
-            <h3>Popular Mytineraries</h3>
+            <h3 className="text-center">Popular Mytineraries</h3>
             <Slider/>
           </div>
         </div>
         </div>
       </section>
 
-      <section>
-        
-        
-      </section>
+      
     </>
   );
 }
