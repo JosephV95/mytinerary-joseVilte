1b81d8601cdedaac16a10be214adadf723947e51
JosephV95 <viltejl@gmail.com>
2023-08-05 00:48:52 -0300

    Se esta mejorando el carousel

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -3,7 +3,7 @@ import "./slider.css";
 
 export default function Slider() {
   return (
-    <div className="container ">
+    <div className="container my-2">
       <div className="row justify-content-center">
         <div className="col-md-9">
           <Carousel>
@@ -11,97 +11,87 @@ export default function Slider() {
               <div className="container  item4Carousel">
                 <div className="row ">
                   <div className="col-6">
-                    <div className="img-caro py-1 m-0">
+                    <div className="sliderItem py-2 m-0 ">
                       <img src="../Akihabara.jpg" alt="" />
+                      <div className="text-img">
+                        <h5>Mümliswil-Ramiswil</h5>
+                        <p>Japón</p>
+                      </div>
                     </div>
                   </div>
                   <div className="col-6">
-                    <div className="img-caro py-1 m-0">
+                    <div className="sliderItem py-2 m-0 ">
                       <img src="../Big ben.jpg" alt="" />
                     </div>
                   </div>
                   <div className="col-6">
-                    <div className="img-caro py-1 m-0">
+                    <div className="sliderItem py-2 m-0 ">
                       <img src="../kioto.jpg" alt="" />
                     </div>
                   </div>
                   <div className="col-6">
-                    <div className="img-caro py-1 m-0">
+                    <div className="sliderItem py-2 m-0 ">
                       <img src="../kioto pagoda.jpg" alt="" />
                     </div>
                   </div>
                 </div>
               </div>
-              <Carousel.Caption>
-                {/* <h3>First slide label</h3>
-                <p>
-                  Nulla vitae elit libero, a pharetra augue mollis interdum.
-                </p> */}
-              </Carousel.Caption>
             </Carousel.Item>
+            
             <Carousel.Item interval={1000}>
               <div className="container  item4Carousel">
                 <div className="row">
                   <div className="col-6">
-                    <div className="img-caro py-1">
+                    <div className="sliderItem py-2 m-0">
                       <img src="../Akihabara.jpg" alt="" />
                     </div>
                   </div>
-                  <div className="col-6 bg-danger">
-                    <div className="img-caro py-1">
+                  <div className="col-6 ">
+                    <div className="sliderItem py-2 m-0">
                       <img src="../Mümliswil-Ramiswil - Suiza.jpg" alt="" />
                     </div>
                   </div>
                   <div className="col-6">
-                    <div className="img-caro py-1">
+                    <div className="sliderItem py-2 m-0">
                       <img src="../Mümliswil-Ramiswil - Suiza.jpg" alt="" />
                     </div>
                   </div>
                   <div className="col-6">
-                    <div className="img-caro py-1">
+                    <div className="sliderItem py-2 m-0">
                       <img src="../kioto pagoda.jpg" alt="" />
                     </div>
                   </div>
                 </div>
               </div>
-              <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </Carousel.Caption>
             </Carousel.Item>
+
             <Carousel.Item interval={1000}>
               <div className="container  item4Carousel">
                 <div className="row">
                   <div className="col-6">
-                    <div className="img-caro py-1">
+                    <div className="sliderItem py-2 m-0">
                       <img src="../Akihabara.jpg" alt="" />
                     </div>
                   </div>
                   <div className="col-6">
-                    <div className="img-caro py-1">
+                    <div className="sliderItem py-2 m-0">
                       <img src="../Mümliswil-Ramiswil - Suiza.jpg" alt="" />
                     </div>
                   </div>
                   <div className="col-6">
-                    <div className="img-caro py-1">
+                    <div className="sliderItem py-2 m-0">
                       <img src="../kioto.jpg" alt="" />
                     </div>
                   </div>
                   <div className="col-6">
-                    <div className="img-caro py-1">
+                    <div className="sliderItem py-2 m-0">
                       <img src="../kioto pagoda.jpg" alt="" />
                     </div>
                   </div>
                 </div>
               </div>
-              <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>
-                  Praesent commodo cursus magna, vel scelerisque nisl
-                  consectetur.
-                </p>
-              </Carousel.Caption>
             </Carousel.Item>
+
           </Carousel>
         </div>
       </div>
