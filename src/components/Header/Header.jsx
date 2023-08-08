06222d85d129a34b7aa00a1a25ebba2343b318a3
JosephV95 <viltejl@gmail.com>
2023-08-08 19:29:37 -0300

    Se agrego logo e icono a la web

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,10 +7,12 @@ function Header() {
       <div className="container">
         <div className="row">
           <div className="col-12 d-flex justify-content-between">
-            <h3>My Tinerary</h3>
+            <span className="d-flex align-items-center">
+              <img src="../MyTineraryLogo.png" alt="MTLogo" />
+              <h3 className="ms-1">My Tinerary</h3>
+            </span>
             {/* </div>
           <div className="col-7"> */}
-
             <Nav />
           </div>
         </div>
