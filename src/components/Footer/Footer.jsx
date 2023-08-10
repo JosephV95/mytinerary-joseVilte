d784ff2f0ecb7742fcfd7d923d73631d54e42d6f
JosephV95 <viltejl@gmail.com>
2023-08-10 00:11:35 -0300

    Se edito el footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,26 +1,30 @@
 import NavRouterLink from "../Nav/NavRouterLink";
+import "./footer.css"
 
 export default function Footer() {
   return (
-    <div className="container-fluid bg-dark text-white">
-      <div className="row">
-        <div className="col-4">
-          <div className="py-3 px-5">My Tinerary | 2023</div>
-        </div>
-        <div className="col-4">
-          <div className="py-3 px-5">
-            <nav>
-              <ul className="nav d-flex flex-columm">
+    <footer className="container-fluid bg-dark text-white py-2">
+      <div className="row align-items-center">
+        <div className="col-sm-2 col-md-4">
+          <div className=" text-center">
+              <ul className="nav d-flex  flex-column justify-content-center align-items-center ">
                 <NavRouterLink title={"Home"} url={"/"} />
                 <NavRouterLink title={"Cities"} url={"/cities"} />
               </ul>
-            </nav>
           </div>
         </div>
-        <div className="col-4">
-          <div className="py-3 px-5">My Tinerary | 2023</div>
+        <div className="col-sm-5 col-md-4 pb-2">
+          <div className="text-center">2023 - My Tinerary</div>
+        </div>
+        <div className="col-sm-5 col-md-4 d-flex justify-content-center align-items-center " >
+            <p className="fw-bold">Follow us:
+            <span className="d-inline-block">
+            <a href="https://es-la.facebook.com/" target="_blank" rel="noreferrer"><i className="fa-brands fa-facebook-f py-1 mx-2 socialLink bg-primary"></i></a>
+            <a href="https://twitter.com/?lang=es" target="_blank" rel="noreferrer"><i className="fa-brands fa-x-twitter py-1 mx-2 socialLink"></i></a>
+            </span>
+            </p>
         </div>
       </div>
-    </div>
+    </footer>
   );
 }
