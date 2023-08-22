e8fca8214979d5652f7c4be11b8c177cffff4993
JosephV95 <viltejl@gmail.com>
2023-08-22 18:49:55 -0300

    Se agrego cityDetails al router

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import MainLayout from "./layouts/MainLayout";
 import Cities from "./pages/Cities/Cities";
 import Home from "./pages/Home";
+import CityDetails from "./pages/CityDetail/CityDetail"
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
   {path:'/', element: <MainLayout />, children:[
     {path:'/', element: <Home/>},
-    {path:'/cities', element: <Cities />}
+    {path:'/cities', element: <Cities />},
+    {path:'/cities/:id', element: <CityDetails/>}
   ]},
 ])
 
