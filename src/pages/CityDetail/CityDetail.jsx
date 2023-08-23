ac3bf411a6bc162462754784ea5aecc3243f9066
JosephV95 <viltejl@gmail.com>
2023-08-22 21:08:24 -0300

    Se creo la page cityDetails y se conecto con el back

---
+++ src/pages/CityDetail/CityDetail.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getCity } from "../../services/CitiesService";
+import "./cityDetail.css";
+
+export default function CityDetail() {
+  const [city, setCity] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    getCity(id).then((res) => setCity(res));
+  }, [id]);
+
+  return (
+    <section className="py-0">
+      <div className="container-fluid">
+        <div
+          className="row justify-content-center detailCity"
+          style={{ backgroundImage: `url(${city.img})`, height: "60vh" }}
+        >
+          <div className="col-12 col-md-6 heroDetail  text-center text-white d-flex flex-column justify-content-center ">
+            <h2 className="py-4">{city.city}</h2>
+            <p>{city.nation}</p>
+            <p>{city.description}</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
+              voluptas ipsam itaque placeat ullam fuga. Corrupti, dolorem
+              pariatur modi sint veritatis vel autem hic, molestias natus sed
+              eum provident repellendus!
+            </p>
+
+            <button type="button" className="btn btn-primary my-3">View itineraries</button>
+          </div>
+        </div>
+
+        <div className="row ">
+          <div className="col-12  py-5 my-5">
+            <h3 className="text-center">Site on construction!</h3>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/services/CitiesService.js
+++ src/services/CitiesService.js
@@ -1,6 +1,6 @@
 // import { useState, useEffect } from "react";
 
-export const CitiesService = async () => {
+export const getAllCities = async () => {
   try {
     const {allCities}  = await fetch("http://localhost:4000/api/cities")
       .then((res) => res.json())
@@ -9,5 +9,17 @@ export const CitiesService = async () => {
     return allCities
   } catch (error) {
     return [];
+  }
+}
+
+export const getCity = async (id) =>{
+  try {
+    const city  = await fetch("http://localhost:4000/api/cities/" +id)
+      .then((res) => res.json())
+      .then()
+
+      return city.oneCity;
+  } catch (error) {
+    return []
   }
 }
