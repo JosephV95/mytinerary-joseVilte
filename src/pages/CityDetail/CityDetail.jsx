f83d1c9c79389198294e30858f7fb4394a12405e
JosephV95 <viltejl@gmail.com>
2023-09-03 14:53:52 -0300

    Se esta dando estilos al cityDetails

--- src/pages/CityDetail/CityDetail.jsx
+++ src/pages/CityDetail/CityDetail.jsx
@@ -7,59 +7,86 @@ import itinerariesActions from "../../store/actions/itinerariesAction";
 
 export default function CityDetail() {
   const { id } = useParams();
-  const cityInStore = useSelector(store => store.citiesReducer.city)
-  const dispatch = useDispatch()
-  const ityStore = useSelector(store => store.itinerariesReducer.itineraries)
+  const cityInStore = useSelector((store) => store.citiesReducer.city);
+  const dispatch = useDispatch();
+  const itineraryStore = useSelector(
+    (store) => store.itinerariesReducer.itineraries
+  );
 
   useEffect(() => {
     dispatch(citiesActions.get_one_city(id));
     dispatch(itinerariesActions.get_itineraries(id));
     // console.log(cityInStore._itineraries);
+    // console.log(itineraryStore);
 
-    
-    console.log(ityStore);
     // return ()=> dispatch(itinerariesActions.reset_ity()) //! dispatch para reiniciar valores al salir o cambiar de pagina
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
-    <section className="py-0">
-      <div className="container-fluid">
-        
-            <div 
-            className="row justify-content-center detailCity"
-            style={{ backgroundImage: `url(${cityInStore.img})`, height: "75vh" }}
-          >
-            <div className="col-12 col-md-6 heroDetail  text-center text-white d-flex flex-column justify-content-center ">
-              <h2 className="py-4">{cityInStore.city}</h2>
-              <p>{cityInStore.nation}</p>
-              <p>{cityInStore.description}</p>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
-                voluptas ipsam itaque placeat ullam fuga. Corrupti, dolorem
-                pariatur modi sint veritatis vel autem hic, molestias natus sed
-                eum provident repellendus!
-              </p>
+    <section className="py-0 bg-info">
+      <div className="container-fluid ">
+        <div
+          className="row justify-content-center detailCity"
+          style={{ backgroundImage: `url(${cityInStore.img})`, height: "75vh" }}
+        >
+          <div className="col-12 col-md-6 heroDetail  text-center text-white d-flex flex-column justify-content-center ">
+            <h2 className="py-4">{cityInStore.city}</h2>
+            <p>{cityInStore.nation}</p>
+            <p>{cityInStore.description}</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
+              voluptas ipsam itaque placeat ullam fuga. Corrupti, dolorem
+              pariatur modi sint veritatis vel autem hic, molestias natus sed
+              eum provident repellendus!
+            </p>
 
-              <button type="button" className="btn btn-primary my-3">View itineraries</button>
-            </div>
+            <button type="button" className="btn btn-primary my-3">
+              View itineraries
+            </button>
           </div>
-          {ityStore.length>0 ?(
-              ityStore.map((iti, key)=>(
-                <h5 key={key}>{iti.name}</h5>
-              ))
-          ) :(
+        </div>
+      </div>
+
+      <div className="container  " >
+        <div className="row  gap-4 py-5">
+
+          {itineraryStore.length > 0 ? (
+            itineraryStore.map((itinerary, key) => (
+              <>
+                <div className="col-12    bg-danger " key={key}>
+                  <div className="row ">
+                    <div
+                      className="col-md-5  text-white text-center"
+                      style={{
+                        backgroundImage: `url(${itinerary.img})`,
+                        backgroundPosition: "center",
+                        backgroundSize: "cover",
+                        minHeight: "15rem",
+                      }}
+                    >
+                      {/* <div className="bg-dark "> */}
+                        <h3 className="pt-2" style={{textShadow:'0 0 15px gray,0 0 15px gray,0 0 20px gray,0 0 20px black'}}>{itinerary.name}</h3>
+                      {/* </div> */}
+                      
+                    </div>
+                    <div className="col-md-7 text-white py-3 px-4">
+                      <p>{itinerary.desc}</p>
+                      <p><b>Price: </b> {itinerary.price ==0 ? "Free" : "$"+itinerary.price}</p>
+                      <p><b>Duration:</b> {itinerary.duration}</p>
+                      <p><b>Likes:</b> ♥ {itinerary.likes}</p>
+                      {/* <p><b>Comments:</b> {itinerary.comments}</p> */}
+                      {itinerary.hastag.map((val)=>(<a key={val.key}> {val}</a> ))}
+                      {/*  */}
+                    </div>
+                  </div>
+                </div>
+              </>
+            ))
+          ) : (
             <h3>No hay itinerarios</h3>
-          )
-        }
-
-          <div className="row ">
-            <div className="col-12  py-5 my-5">
-              <h3 className="text-center">Site on construction!</h3>
-            </div>
-          </div>
-          
-        
+          )}
+        </div>
       </div>
     </section>
   );
