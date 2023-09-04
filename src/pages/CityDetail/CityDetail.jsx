9630c798b1b3a4058c815368f20e5f97831fb153
JosephV95 <viltejl@gmail.com>
2023-09-04 01:54:46 -0300

    Se cambio estilos para details

--- src/pages/CityDetail/CityDetail.jsx
+++ src/pages/CityDetail/CityDetail.jsx
@@ -41,7 +41,7 @@ export default function CityDetail() {
               eum provident repellendus!
             </p>
 
-            <button type="button" className="btn btn-primary my-3">
+            <button type="button" className="btn btn-primary my-3"  onClick={()=>{}}>
               View itineraries
             </button>
           </div>
@@ -50,14 +50,15 @@ export default function CityDetail() {
 
       <div className="container  " >
         <div className="row  gap-4 py-5">
+          <h2 className="text-center text-white" id="itineraries"><i>Itineraries</i></h2>
 
           {itineraryStore.length > 0 ? (
             itineraryStore.map((itinerary, key) => (
               <>
-                <div className="col-12    bg-danger " key={key}>
-                  <div className="row ">
+                <div className="col-12  rounded-5 bg-danger " key={key}>
+                  <div className="row justify-content-center">
                     <div
-                      className="col-md-5  text-white text-center"
+                      className="col-md-5  text-white d-flex flex-column justify-content-between  rounded-start-3"
                       style={{
                         backgroundImage: `url(${itinerary.img})`,
                         backgroundPosition: "center",
@@ -65,26 +66,35 @@ export default function CityDetail() {
                         minHeight: "15rem",
                       }}
                     >
-                      {/* <div className="bg-dark "> */}
-                        <h3 className="pt-2" style={{textShadow:'0 0 15px gray,0 0 15px gray,0 0 20px gray,0 0 20px black'}}>{itinerary.name}</h3>
-                      {/* </div> */}
+                      <div className="">
+                        <h3 className="pt-2  text-center" style={{textShadow:'0 0 15px gray,0 0 15px gray,0 0 20px gray,0 0 20px black'}}>{itinerary.name}</h3>
+                      </div>
+                      <div>
+                        <h5 className="text-end"> <i className="fa-regular fa-circle-user"></i> Username</h5>
+                      </div>
                       
                     </div>
                     <div className="col-md-7 text-white py-3 px-4">
                       <p>{itinerary.desc}</p>
-                      <p><b>Price: </b> {itinerary.price ==0 ? "Free" : "$"+itinerary.price}</p>
+                      <p><b>Price: </b>  {itinerary.price ==0 ? "Free" : "💸"+itinerary.price}</p>
                       <p><b>Duration:</b> {itinerary.duration}</p>
                       <p><b>Likes:</b> ♥ {itinerary.likes}</p>
                       {/* <p><b>Comments:</b> {itinerary.comments}</p> */}
-                      {itinerary.hastag.map((val)=>(<a key={val.key}> {val}</a> ))}
-                      {/*  */}
+                      <p><b>Tags:</b>{itinerary.hastag.map((val)=>(<a key={val.key}> {val}</a> ))}  </p>
+                      
                     </div>
                   </div>
                 </div>
               </>
             ))
           ) : (
-            <h3>No hay itinerarios</h3>
+            <>
+                  <div className="col-12  text-center">
+                    <img src="../sorryIco.png" alt="no results" style={{width:'12rem'}} />
+                    <h2 className="text-center">No itineraries yet.</h2>
+                  </div>
+          
+            </>
           )}
         </div>
       </div>
