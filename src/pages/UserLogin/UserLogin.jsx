fafa4f97fc1352a9db86eda70d610741975bc5ab
JosephV95 <viltejl@gmail.com>
2023-09-13 19:06:40 -0300

    Se completo funcionamienro del Login

--- src/pages/UserLogin/UserLogin.jsx
+++ src/pages/UserLogin/UserLogin.jsx
@@ -1,32 +1,40 @@
 // import axios from "axios";
 import { useRef, useState } from "react";
 import { Button, Form } from "react-bootstrap";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import userActions from "../../store/actions/authActions";
+import { useNavigate } from "react-router-dom";
 
 export default function UserLogin() {
 
   const dispatch = useDispatch();
+  const navigate = useNavigate()
+  const userLogged = useSelector(store => store.userReducer.isLogged)
 
     const userEmailRef = useRef();
     const userPasswordRef = useRef();
 
     const [validated, setValidated] = useState(false);
 
+    if (userLogged) {
+       return navigate("/")
+    }
+
     const handleSubmit = (event) => {
         event.preventDefault();
-        // const form = event.currentTarget;
-        // if (form.checkValidity() === false) {
-        // event.preventDefault();
-        // event.stopPropagation();
-        // } 
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+        } 
+        setValidated(true);
 
         dispatch(userActions.user_login({
           email: userEmailRef.current.value,
           password: userPasswordRef.current.value
         }))
 
-      setValidated(true);
+      
   };
 
   return (
@@ -37,14 +45,14 @@ export default function UserLogin() {
             <h1>Mytinerary</h1>
             <p>Welcome nuevo usuario</p>
           </div>
-          <div className="col-md-7">
-            <Form className="  p-4 rounded"   validated={validated} onSubmit={handleSubmit}  style={{backgroundColor: "white"}}>
-              <h3 className="text-center mb-4"><i className="fa-solid fa-user-plus" style={{color:"#0ec06d"}}></i> Login</h3>
+          <div className="col-md-7 rounded"  style={{backgroundColor: "white"}}>
+            <Form className="  p-4 rounded"   validated={validated} onSubmit={handleSubmit}  >
+              <h3 className="text-center mb-4"><i className="fa-solid fa-user-check" style={{color:"#0bab6d"}}></i> Sign In</h3>
               <fieldset>
                 
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                   <Form.Label>Email address:</Form.Label>
-                  <Form.Control type="email" placeholder="Enter email" aria-describedby=""  ref={userEmailRef} required />
+                  <Form.Control type="email" placeholder="Enter email" aria-describedby="" pattern=".+com"  ref={userEmailRef} required />
                 </Form.Group>
 
                 <Form.Group className="mb-3">
@@ -53,19 +61,22 @@ export default function UserLogin() {
                     type="password"
                     pattern=".{6,12}"
                     id="inputPassword5"
-                    aria-describedby="pass" placeholder="Password"  ref={userPasswordRef} required 
+                    aria-describedby="pass" placeholder="Enter Password"  ref={userPasswordRef} required 
                   />
                   <Form.Text id="pass" muted>
                     The password must be 6-12 characters
                   </Form.Text>
                 </Form.Group>
                 
-                <Button type="submit" className="mt-2 btn btn-success">Login</Button>
+                <Button type="submit" className="mt-2 btn btn-primary">Sign In</Button> or <Button  className="mt-2 btn btn-secondary">ingresar con google</Button>
               </fieldset>
-              <div>
-                <a href="">Ingresar con Google</a>
-              </div>
+              
             </Form>
+            <div className="w-100 mb-4 mt-3 pt-0 d-flex justify-content-center align-items-center">
+              You do not have an account? 
+              <Button type="button" className=" btn btn-success ms-2"  onClick={()=>{navigate("/user/register")}}>Sign up</Button>
+            </div>
+
           </div>
         </div>
       </div>
