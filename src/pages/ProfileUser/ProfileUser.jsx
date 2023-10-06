12440c83a4fb2d70f251886adee0a2371e8c742d
JosephV95 <viltejl@gmail.com>
2023-10-06 20:03:21 -0300

    Se busca mostrar la info del user en los inputs

--- src/pages/ProfileUser/ProfileUser.jsx
+++ src/pages/ProfileUser/ProfileUser.jsx
@@ -1,17 +1,50 @@
+import { useEffect, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import { useSelector } from "react-redux";
 
 export default function ProfileUser() {
-  return (
-   <Container>
-    <Row md={2}>
-      <Col>
-      <p>Me dicen antorcha</p>
-      </Col>
+  const userInStore = useSelector(store => store.userReducer.user);
+  const [userStore, setUserStore] = useState(userInStore);
 
-      <Col md={{span:3,offset: 2}}>
-        <p>Hola mamu Raikou</p>
-      </Col>
-    </Row>
-   </Container>
-  )
+  console.log(userInStore);
+  
+  useEffect(()=>{
+    setUserStore(userInStore)
+    console.log(userInStore);
+    // console.log(userName.current);
+  },[])
+  // const userLastname = useRef(userInStore.lastname)
+  // const userPhoto = useRef(userInStore.photo)
+  // const userPassword = useRef(userInStore.password)
+
+  return (
+    <section>
+      <Container>
+        <Row >
+            <Col md={3}>
+              <p>Name: </p>
+            </Col>
+            <Col md={8}>
+              <input type="text" name="" value={userStore.name} />
+            </Col>
+            <Col md={3}>
+              <p>Lastname: </p>
+            </Col>
+            <Col md={8}>
+              <input type="text" name="" value={userStore.password}/>
+            </Col>
+            <Col md={3}>
+              <p>Photo: </p>
+            </Col>
+            <Col md={8}>
+              <input type="text" name="" value={userStore.photo}/>
+            </Col>
+
+          <Col md={{ span: 3, offset: 2 }}>
+            <p>Hola mamu Raikou</p>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
 }
