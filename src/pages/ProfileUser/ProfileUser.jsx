5c30782de46822ffba2a20adb0d7d77e503b00ea
JosephV95 <viltejl@gmail.com>
2023-10-12 18:54:04 -0300

    Se mejoro el funcionamiento de los inputs

--- src/pages/ProfileUser/ProfileUser.jsx
+++ src/pages/ProfileUser/ProfileUser.jsx
@@ -1,24 +1,29 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import { useSelector } from "react-redux";
 
 export default function ProfileUser() {
   const userInStore = useSelector(store => store.userReducer.user);
   const [userStore, setUserStore] = useState(userInStore);
+  const [profilePhoto, setProfilePhoto] = useState("");
 
-  console.log(userInStore);
+  const upUserName = useRef();
+  const upUserLastname = useRef();
+  const upUserPhoto = useRef();
   
   useEffect(()=>{
     setUserStore(userInStore)
-    console.log(userInStore);
+    setProfilePhoto(userInStore.photo)
     // console.log(userName.current);
   },[userInStore])
-  // const userLastname = useRef(userInStore.lastname)
-  // const userPhoto = useRef(userInStore.photo)
-  // const userPassword = useRef(userInStore.password)
+
+  const changePhoto = (e)=>{
+    console.log(e.target.value);
+    setProfilePhoto(e.target.value)
+  }
 
   return (
-    <section>
+    <section style={{minHeight:"83vh"}}>
       <Container>
         <Row  className="justify-content-center">
           <Col md={{ span: 7 }}>
@@ -27,7 +32,7 @@ export default function ProfileUser() {
              
              <Row className="justify-content-center mb-4">
               <div className="border border-light border-2 rounded-circle" style={{width:"200px", height:"200px", 
-                  backgroundImage:`url(${userInStore.photo})`, backgroundPosition:"center", backgroundSize:"cover"}}>
+                  backgroundImage:`url(${profilePhoto})`, backgroundPosition:"center", backgroundSize:"cover"}}>
               </div>
              </Row>
               <fieldset>
@@ -38,32 +43,29 @@ export default function ProfileUser() {
                       <Form.Control
                         type="text"
                         id="inputPassword5"
-                        aria-describedby="pass"  defaultValue={userStore.name}
-                      />
+                        aria-describedby="pass"  defaultValue={userStore.name} ref={upUserName}  />
                   </Form.Group>
                   <Form.Group className="mb-3" as={Col} md="6">
                         <Form.Label htmlFor="inputLastname">Lastname:</Form.Label>
                         <Form.Control
                           type="text"
                           id="inputLastname"
-                          aria-describedby="textLastName"  defaultValue={userStore.lastname}
-                        />
-                  </Form.Group>
+                          aria-describedby="textLastName"  defaultValue={userStore.lastname} ref={upUserLastname}  />
+                  </Form.Group> 
                 </Row>
 
                 <Form.Group className="mb-3">
                       <Form.Label htmlFor="photoUrl">User photo: </Form.Label>
-                      <Form.Control type="url" id="photoUrl" placeholder="Image url"  defaultValue={userStore.photo} />
+                      <Form.Control type="url" id="photoUrl" placeholder="Image url"  
+                      defaultValue={userStore.photo} onChange={changePhoto} ref={upUserPhoto}/>
                 </Form.Group>
 
                 <div className="d-flex justify-content-center mt-4">
-                  <Button type="submit" variant="outline-warning" > <i className="fa-solid fa-floppy-disk"></i> Update</Button>
+                  <Button type="submit" variant="primary" > <i className="fa-solid fa-floppy-disk" style={{fontSize:"1.28rem"}}></i> Update</Button>
                 </div>
               </fieldset>
             </Form>
           </Col> 
-
-          
         </Row>
       </Container>
     </section>
