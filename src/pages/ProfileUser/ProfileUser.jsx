e5230617ecb84126c9e99cc65dfe68110ff96beb
JosephV95 <viltejl@gmail.com>
2023-10-12 00:47:31 -0300

    Se esta editando los inputs

--- src/pages/ProfileUser/ProfileUser.jsx
+++ src/pages/ProfileUser/ProfileUser.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Col, Container, Form, Row } from "react-bootstrap";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import { useSelector } from "react-redux";
 
 export default function ProfileUser() {
@@ -20,40 +20,45 @@ export default function ProfileUser() {
   return (
     <section>
       <Container>
-        <Row >
-          <Col md={{ span: 6, offset: 3 }}>
-            <Form  className="p-4 rounded" >
-              <h3 className="text-center mb-4"><i className="fa-solid fa-user-plus" style={{color:"#0bab6d"}}></i> Update Data</h3>
+        <Row  className="justify-content-center">
+          <Col md={{ span: 7 }}>
+            <Form  className="p-4 rounded-5 text-white position-relative" style={{backgroundColor:"rgb(21, 123, 141)"}}>
+              <h3 className="text-center mb-3"><i className="fa-solid fa-pen-to-square" style={{color:"#2dfb6b"}}></i> Edit Profile</h3>
+             
+             <Row className="justify-content-center mb-4">
+              <div className="border border-light border-2 rounded-circle" style={{width:"200px", height:"200px", 
+                  backgroundImage:`url(${userInStore.photo})`, backgroundPosition:"center", backgroundSize:"cover"}}>
+              </div>
+             </Row>
               <fieldset>
-                <Form.Group>
+                <Row>
+                  {/* //todo  Se utilizara defaultValue en vez de solo value, para no lanzar errores por consola  */}
+                  <Form.Group className="mb-3" as={Col} md="6">
                       <Form.Label htmlFor="inputPassword5">Name:</Form.Label>
                       <Form.Control
                         type="text"
-                        pattern=".{6,12}"
                         id="inputPassword5"
-                        aria-describedby="pass" required  value={userStore.name}
+                        aria-describedby="pass"  defaultValue={userStore.name}
                       />
-                  {/* <Col md={3}>
-                    <p>Lastname: </p>
-                  </Col>
-                  <Col md={8}>
-                    <input type="text" name="" value={userStore.lastname}/>
-                  </Col> */}
-                </Form.Group>
-                <Form.Group>
-                      <Form.Label htmlFor="inputLastname">Lastname:</Form.Label>
-                      <Form.Control
-                        type="text"
-                        pattern=".{6,12}"
-                        id="inputLastname"
-                        aria-describedby="textLastName" required  value={userStore.lastname}
-                      />
-                </Form.Group>
+                  </Form.Group>
+                  <Form.Group className="mb-3" as={Col} md="6">
+                        <Form.Label htmlFor="inputLastname">Lastname:</Form.Label>
+                        <Form.Control
+                          type="text"
+                          id="inputLastname"
+                          aria-describedby="textLastName"  defaultValue={userStore.lastname}
+                        />
+                  </Form.Group>
+                </Row>
 
                 <Form.Group className="mb-3">
                       <Form.Label htmlFor="photoUrl">User photo: </Form.Label>
-                      <Form.Control type="url" id="photoUrl" placeholder="Image url" required  value={userStore.photo} />
+                      <Form.Control type="url" id="photoUrl" placeholder="Image url"  defaultValue={userStore.photo} />
                 </Form.Group>
+
+                <div className="d-flex justify-content-center mt-4">
+                  <Button type="submit" variant="outline-warning" > <i className="fa-solid fa-floppy-disk"></i> Update</Button>
+                </div>
               </fieldset>
             </Form>
           </Col> 
