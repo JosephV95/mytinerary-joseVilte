f781eb7bab2b77f8c38bb8720d67149d2da98157
JosephV95 <viltejl@gmail.com>
2023-10-27 21:13:29 -0300

    Se agrego alerta al crear un city

--- src/store/actions/citiesAction.js
+++ src/store/actions/citiesAction.js
@@ -1,5 +1,16 @@
 import { createAction, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+import Swal from "sweetalert2";
+
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'top',
+    showConfirmButton: false,
+    timer: 1500,
+    didOpen: (toast) => {
+      toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+  })
 
 //* Redux Asincrono   con el await le digo a la funcion que la promesa se debe resolver primero y luego continuar con lo demas
 const get_cities = createAsyncThunk('get_cities', async(queries= "")=>{    //! El asyncThunk trae por defecto el payload, por lo que no se necesita definirlo
@@ -39,7 +50,11 @@ const create_city = createAsyncThunk('create_city', async(dataNewCity)=>{
     try {
         const newCity = await axios.post("http://localhost:4000/api/cities", dataNewCity)
         .then((res)=> {
-            console.log(res.data);
+            // console.log(res.data);
+            Toast.fire({
+                icon: 'success',
+                title: 'The city ' + res.data.newCity.city + ' was created'
+            })
             return res.data
         })
         
@@ -53,11 +68,9 @@ const create_city = createAsyncThunk('create_city', async(dataNewCity)=>{
 const cities_api = createAction('cities_api', (payload)=>{
     return { payload }
 } )
-
 const all_cities = createAction('all_cities', (payload)=>{
     return { payload }
 })
-
 
 const citiesActions = {cities_api, all_cities,  get_cities, get_one_city, create_city}
 
