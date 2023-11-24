d1f98f10905d13c5963f2979322148e38356b4fe
JosephV95 <viltejl@gmail.com>
2023-11-24 12:35:04 -0300

    Se reemplazo la url por la creada en el backend deploy

--- src/store/actions/itinerariesAction.js
+++ src/store/actions/itinerariesAction.js
@@ -12,7 +12,8 @@ const Toast = Swal.mixin({
     }
 })
 
-const urlApi = "http://localhost:4000/api"
+// const urlApi = "http://localhost:4000/api" //todo  esta seria la url para el trabajo local
+const urlApi = "mytinerary-back-viltejose-production.up.railway.app/api" //! url del deploy  del back(le agregue el/api para el correcto funcionamiento)
 
 const get_itineraries = createAsyncThunk('get_itineraries', async(id)=>{
     try {
