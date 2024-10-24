312896ae7803e0fc5c1b52df08194cdbf29a0fea
JosephV95 <viltejl@gmail.com>
2024-10-24 20:33:12 -0300

    Se creo rama para mejorar diseño de la page

--- src/utils/urlApiBackend.js
+++ src/utils/urlApiBackend.js
@@ -1,6 +1,6 @@
 
-// export const urlApi = "http://localhost:4000/api" //todo  esta seria la url para el trabajo local
-export const urlApi = "https://mytinerary-back-vilte-jose.vercel.app/api" //! url del deploy  del back(le agregue el/api para el correcto funcionamiento)
+export const urlApi = "http://localhost:4000/api" //todo  esta seria la url para el trabajo local
+// export const urlApi = "https://mytinerary-back-vilte-jose.vercel.app/api" //! url del deploy  del back(le agregue el/api para el correcto funcionamiento)
 
     
  
