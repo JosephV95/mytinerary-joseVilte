c612d4136e1ec3803bfd04c5ae4f88aa3b3dfd6b
JosephV95 <viltejl@gmail.com>
2024-10-24 21:49:23 -0300

    Se comenzo a editar el diseño

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,18 +5,17 @@ import "./main.css";
 function Main() {
   return (
     <>
-      <section className="py-4  main" > {/*style={{backgroundImage:"url('https://images.unsplash.com/photo-1606820854416-439b3305ff39?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')", backgroundSize:"cover", backgroundPosition:"center"}} */}
-        <h4 className="my-4 text-center px-3 fst-italic" data-aos="zoom-out" data-aos-duration="1500" data-aos-delay="1000">Find your perfect trip, designed by insiders who know and love their cities!</h4>
+      <section className="py-5  main" > {/*style={{backgroundImage:"url('https://images.unsplash.com/photo-1606820854416-439b3305ff39?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')", backgroundSize:"cover", backgroundPosition:"center"}} */}
+        <h4 className="my-4 text-center px-3 fst-italic" data-aos="zoom-out" data-aos-duration="500" data-aos-delay="500">Find your perfect trip, designed by insiders who know and love their cities!</h4>
         <div className="container" >
           <div className="row hero  justify-content-center align-items-center my-3" data-aos="fade-down" data-aos-duration="1000">
             <div className="hero-article col-md-7 text-center text-white">
               <div className="p-0 pb-3 mt-3">
-                <h3>Discover your perfect city</h3>
+                <h3>Con Mitinerary encontraras el destino perfecto para tu viaje</h3>
                 <p className="pb-1">
-                  Our app will help you find the perfect path for your next
-                  trip. With an easy-to-use interface and a host of itinerary
-                  options, planning your next trip has never been easier.
+                  MyTinerary te ayudará a descubrir los mejores lugares y asi aprovechar al máximo tu viaje.
                 </p>
+                <p>Contamos con una gran cantidad de itinerarios de las distintas ciudades del mundo, planificar su próximo viaje nunca ha sido tan fácil.</p>
 
                 <CallToAction />
               </div>
