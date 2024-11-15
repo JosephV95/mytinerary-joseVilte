2e05428d266e5253bd001839efe6446da1abddbb
JosephV95 <viltejl@gmail.com>
2024-11-15 19:25:44 -0300

    Se mejoro contenido

--- src/components/CallToAction/CallToAction.jsx
+++ src/components/CallToAction/CallToAction.jsx
@@ -4,8 +4,8 @@ import "./style.css";
 export default function CallToAction() {
   return (
     <div className="row justify-content-center mt-4 mb-1 mx-1">
-        <PageRouter to={'/cities'}  className="btn btn-outline-primary buttonCall px-0 text-white"> 
-        <i className=" " ></i> Discover Cities!
+        <PageRouter to={'/cities'}  className="btn btn-outline-primary buttonCall px-5  text-white"> 
+          Ver todas las Ciudades
         </PageRouter>
     </div>
   );
