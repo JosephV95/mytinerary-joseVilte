cbb4ae13bc0b62a108f11a41ff6775342831ddd6
JosephV95 <viltejl@gmail.com>
2024-11-21 00:44:17 -0300

    Se mejoro estilos

--- src/pages/Cities/Cities.jsx
+++ src/pages/Cities/Cities.jsx
@@ -112,7 +112,7 @@ export default function Cities() {
                   <Card.ImgOverlay className="d-flex position-absolute overflow-hidden">
                     <div className="bg-dark bg-opacity-75 position-absolute top-0 start-0 w-100 text-center">
                       <Card.Title className="m-0 ">{city.city}</Card.Title>
-                      <Card.Text><i className="fa-solid fa-location-dot " style={{fontSize:"12.8px"}}></i> {city.nation}</Card.Text>
+                      <Card.Text className="fst-italic"><i className="fa-solid fa-location-dot" style={{fontSize:"12.5px"}}></i> {city.nation}</Card.Text>
                     </div>
                     
                     <div className="divInfoCity position-absolute bottom-0 w-100 h-60 px-2 pb-1">
